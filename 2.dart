4aaab8e5dbe74bb25ea68c1fea8a4530fc8e620c
Momoz-17 <mohitvijaygupta17@gmail.com>
2024-12-17 12:48:03 +0530

    Simple Arithmetic Operations

---
+++ 2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 10;
+  int b = 5;
+  int Sum = a + b;
+  int Difference = a - b;
+  int Product = a * b;
+  double Quotient = a / b;
+  print('Sum: $Sum');
+  print('Difference: $Difference');
+  print('Product: $Product');
+  print('Quotient: $Quotient');
+}
